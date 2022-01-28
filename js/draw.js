6728729d5b29274333d12fb7e4ce30119a9dc3e6
mccalluc <mccalluc@users.noreply.github.com>
2022-01-27 20:23:47 -0500

    triangle

--- js/draw.js
+++ js/draw.js
@@ -1,124 +1,64 @@
-var canvas = this.__canvas = new fabric.Canvas('c');
-// create a polygon object
-var points = [{
-  x: 3, y: 4
-}, {
-  x: 16, y: 3
-}, {
-  x: 30, y: 5
-},  {
-  x: 25, y: 55
-}, {
-  x: 19, y: 44
-}, {
-  x: 15, y: 30
-}, {
-  x: 15, y: 55
-}, {
-  x: 9, y: 55
-}, {
-  x: 6, y: 53
-}, {
-  x: -2, y: 55
-}, {
-  x: -4, y: 40
-}, {
-  x: 0, y: 20
-}]
-var polygon = new fabric.Polygon(points, {
-  left: 100,
-  top: 50,
-  fill: '#D81B60',
-  strokeWidth: 4,
-  stroke: 'green',
-  scaleX: 4,
-  scaleY: 4,
-  objectCaching: false,
-  transparentCorners: false,
-  cornerColor: 'blue',
-});
-canvas.viewportTransform = [0.7, 0, 0, 0.7, -50, 50];
-canvas.add(polygon);
+// Based on https://embed.plnkr.co/o5QHap7NV1SYIbsS76f5/
 
-// define a function that can locate the controls.
-// this function will be used both for drawing and for interaction.
-function polygonPositionHandler(dim, finalMatrix, fabricObject) {
-  var x = (fabricObject.points[this.pointIndex].x - fabricObject.pathOffset.x),
-      y = (fabricObject.points[this.pointIndex].y - fabricObject.pathOffset.y);
-  return fabric.util.transformPoint(
-    { x: x, y: y },
-    fabric.util.multiplyTransformMatrices(
-      fabricObject.canvas.viewportTransform,
-      fabricObject.calcTransformMatrix()
-    )
-  );
-}
+ 
+  
+  export function drawTriangle(x,y,ax,ay,bx,by){
+     var path1 = [["M",x,y],["L",ax,ay],["L",bx,by],["Z"]]; //SVG structure for a typical triangle
+     var discattr = {fill: "#00f",stroke: "none"};
+     
 
-// define a function that will define what the control does
-// this function will be called on every mouse move after a control has been
-// clicked and is being dragged.
-// The function receive as argument the mouse event, the current trasnform object
-// and the current position in canvas coordinate
-// transform.target is a reference to the current object being transformed,
-function actionHandler(eventData, transform, x, y) {
-  var polygon = transform.target,
-      currentControl = polygon.controls[polygon.__corner],
-      mouseLocalPosition = polygon.toLocalPoint(new fabric.Point(x, y), 'center', 'center'),
-      polygonBaseSize = polygon._getNonTransformedDimensions(),
-      size = polygon._getTransformedDimensions(0, 0),
-      finalPointPosition = {
-        x: mouseLocalPosition.x * polygonBaseSize.x / size.x + polygon.pathOffset.x,
-        y: mouseLocalPosition.y * polygonBaseSize.y / size.y + polygon.pathOffset.y
-      };
-  polygon.points[currentControl.pointIndex] = finalPointPosition;
-  return true;
-}
-
-// define a function that can keep the polygon in the same position when we change its
-// width/height/top/left.
-function anchorWrapper(anchorIndex, fn) {
-  return function(eventData, transform, x, y) {
-    var fabricObject = transform.target,
-        absolutePoint = fabric.util.transformPoint({
-            x: (fabricObject.points[anchorIndex].x - fabricObject.pathOffset.x),
-            y: (fabricObject.points[anchorIndex].y - fabricObject.pathOffset.y),
-        }, fabricObject.calcTransformMatrix()),
-        actionPerformed = fn(eventData, transform, x, y),
-        newDim = fabricObject._setPositionDimensions({}),
-        polygonBaseSize = fabricObject._getNonTransformedDimensions(),
-        newX = (fabricObject.points[anchorIndex].x - fabricObject.pathOffset.x) / polygonBaseSize.x,
-        newY = (fabricObject.points[anchorIndex].y - fabricObject.pathOffset.y) / polygonBaseSize.y;
-    fabricObject.setPositionByOrigin(absolutePoint, newX + 0.5, newY + 0.5);
-    return actionPerformed;
+    var paper = Raphael(document.getElementById("canvas"), 0, 0, 200, 200);
+     var triangle = paper.path(path1).attr("fill","#F00"); // Draw the triangle
+     
+     // This piece of code creates all the blue circles on the tops of the triangle and keeps a reference to them in the controls object
+     const controls = paper.set(
+        paper.circle(x, y, 5).attr(discattr),
+        paper.circle(ax, ay, 5).attr(discattr),
+        paper.circle(bx, by, 5).attr(discattr));
+      
+      //Create an update function for each of the controls which manipulate the vertices of path1
+      controls[0].update = function(dx,dy) {
+        var newX = this.attr("cx") + dx; // calculate the new intermediate X value
+        var newY = this.attr("cy") + dy; // calculate the new intermediate Y value
+        this.attr({cx:newX,cy:newY}); // store the intermediate values on the control
+        
+        /*Modify the paths*/
+        path1[0][1] = newX; //the x value of the first vertex in path1
+        path1[0][2] = newY;//the y value of the first vertex int path1
+        
+        triangle.attr({path: path1}); //tell the triangle path to update it's path this will force a redraw on that path
+      }
+      controls[1].update = function(dx,dy) {
+        var newX = this.attr("cx") + dx; // calculate the new intermediate X value
+        var newY = this.attr("cy") + dy; // calculate the new intermediate Y value
+        this.attr({cx:newX,cy:newY}); // store the intermediate values on the control
+         path1[1][1]  = newX; //the x value of the first vertex
+         path1[1][2]  = newY;//the y value of the first vertex
+         
+         triangle.attr({path:path1}); //tell the triangle path to update it's path this will force a redraw on that path
+      }
+      
+      controls[2].update = function(dx,dy) {
+        var newX = this.attr("cx") + dx; // calculate the new intermediate X value
+        var newY = this.attr("cy") + dy; // calculate the new intermediate Y value
+        this.attr({cx:newX,cy:newY}); // store the intermediate values on the control
+         path1[2][1] = newX; //the x value of the first vertex
+         path1[2][2] = newY;//the y value of the first vertex
+         
+         triangle.attr({path:path1}) //tell the triangle path to update it's path this will force a redraw on that path
+      }
+      
+      // set the delegate functions for the drag function 
+      controls.drag(move,up);
   }
-}
-
-function Edit() {
-  // clone what are you copying since you
-  // may want copy and paste on different moment.
-  // and you do not want the changes happened
-  // later to reflect on the copy.
-  var poly = canvas.getObjects()[0];
-  canvas.setActiveObject(poly);
-  poly.edit = !poly.edit;
-  if (poly.edit) {
-    var lastControl = poly.points.length - 1;
-    poly.cornerStyle = 'circle';
-    poly.cornerColor = 'rgba(0,0,255,0.5)';
-    poly.controls = poly.points.reduce(function(acc, point, index) {
-      acc['p' + index] = new fabric.Control({
-        positionHandler: polygonPositionHandler,
-        actionHandler: anchorWrapper(index > 0 ? index - 1 : lastControl, actionHandler),
-        actionName: 'modifyPolygon',
-        pointIndex: index
-      });
-      return acc;
-    }, { });
-  } else {
-    poly.cornerColor = 'blue';
-    poly.cornerStyle = 'rect';
-    poly.controls = fabric.Object.prototype.controls;
+  
+  function move(dx,dy){
+    this.update(dx - (this.dx ||0) ,dy - (this.dy || 0)); // for the control being drug call the update function passing the dx and dy
+    this.dx = dx;
+    this.dy = dy;
   }
-  poly.hasBorders = !poly.edit;
-  canvas.requestRenderAll();
-}
+  
+  function up(){
+    this.dx = this.dy = 0; // reset distance moved x,y with mouse up
+  }
+
