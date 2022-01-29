fecb6c57689c95beaf7455e2c2de5c455d7b12d5
mccalluc <mccalluc@users.noreply.github.com>
2022-01-29 15:40:00 -0500

    less hard-coding

--- js/main.js
+++ js/main.js
@@ -2,24 +2,20 @@
 import { makeCssFontFace, makeFont } from './fonts.js';
 import Canvas from './Canvas.js';
 
-export default function main() {
+export default function main({targetClass, canvasId}) {
   new Canvas({
-    domId: 'canvas',
+    domId: canvasId,
     segments: {
       ABC: [[150, 10], [200, 100], [100, 100], [50, 50]]
     }
   })
 
-  const divId = 'demo';
-  document.getElementById(divId).innerHTML = '0123456789';
-
   const fontName = 'Demo';
   const font = makeFont(fontName);
   const styleSheet = document.createElement('style');
-  styleSheet.type = 'text/css';
   styleSheet.innerText = `
     ${makeCssFontFace(fontName, font)}
-    #${divId} { font-family: '${fontName}' }
+    .${targetClass} { font-family: '${fontName}' }
   `;
   document.head.appendChild(styleSheet);
 }
