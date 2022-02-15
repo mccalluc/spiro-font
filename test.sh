800b7a2606e954d986babb2a5b527c1ad9f8bf4b
mccalluc <mccalluc@users.noreply.github.com>
2022-02-14 21:41:23 -0500

    Add CI

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit
+
+start() { echo "::group::$1"; }
+end() { echo "::endgroup::"; }
+die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
+
+start jest
+cd tests
+npm run test
+cd -
+end jest
