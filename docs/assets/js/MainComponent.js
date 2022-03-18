eb4defe31ec688a13a65f30032382c70678def77
mccalluc <mccalluc@users.noreply.github.com>
2022-03-18 12:29:15 -0400

    computed prop

--- docs/assets/js/MainComponent.js
+++ docs/assets/js/MainComponent.js
@@ -3,11 +3,17 @@ export default {
     baseUrl: String,
     page: Object,
   },
+  computed: {
+    segmentMapToText() {
+      const {segmentMap} = this.page;
+      return Object.entries(segmentMap).map(([from, to]) => `${from} ${to}`).join('\n');
+    }
+  },
   template: `
   <p><a :href="baseUrl">home</a></p>
   <h1>{{ page.name }}</h1>
   <textarea rows="2" columns="12" class="style-me">{{ page.sampleText }}</textarea>
   <button class="style-me" id="download">GET FONT</button>
-  <div id="controls"></div>
+  <textarea rows="10" :value="segmentMapToText" />
   `
 }
