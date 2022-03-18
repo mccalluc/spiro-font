f4adab6e78001f7b8e53fb8aa134ca995d269b7f
mccalluc <mccalluc@users.noreply.github.com>
2022-03-18 01:41:51 -0400

    move rest inside view

--- docs/assets/js/MainComponent.js
+++ docs/assets/js/MainComponent.js
@@ -4,15 +4,10 @@ export default {
     page: Object,
   },
   template: `
-  <div>
-    <p><a :href="baseUrl">home</a></p>
-    <h1>{{ page.name }}</h1>
-    {{ page.name }}
-    <textarea rows="2" columns="12" class="style-me">{{ page.sampleText }}</textarea>
-  </div>
+  <p><a :href="baseUrl">home</a></p>
+  <h1>{{ page.name }}</h1>
+  <textarea rows="2" columns="12" class="style-me">{{ page.sampleText }}</textarea>
+  <button class="style-me" id="download">GET FONT</button>
+  <div id="controls"></div>
   `
 }
-
-// <h1>{{ name }}</h1>
-// {{ content }}
-// <textarea rows="2" columns="12" class="style-me">{{ sampleText }}</textarea>
