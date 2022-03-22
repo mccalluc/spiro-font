422dfeec7dc06eadee5107fbe6949644eaa8dea2
mccalluc <mccalluc@users.noreply.github.com>
2022-03-22 17:23:21 -0400

    add test.sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -o errexit
+
+start() { echo "::group::$1"; }
+end() { echo "::endgroup::"; }
+die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
+
+
+start changelog
+cd docs
+bundler exec jekyll build
+cd -
+end changelog
+
+
+start npm
+npm run test
+end npm
