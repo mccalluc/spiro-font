01cf6537f05a1fff622db64a56e4bff4b7dbf241
Chuck McCallum <mccalluc@users.noreply.github.com>
2022-03-21 21:48:18 -0400

    Use vue (#22)
    * template
    
    * A bit of vue
    
    * move rest inside view
    
    * computed prop
    
    * draw segments
    
    * pass individual props; segments can be moved
    
    * svg utils
    
    * v-model
    
    * more inits
    
    * set font-face THE WRONG WAY!
    
    * shrink grow bevel!
    
    * update segment map
    
    * simpler loop
    
    * force regen
    
    * svg drag works!
    
    * remove unused files

---
+++ docs/assets/js/MainComponent.js
@@ -0,0 +1,86 @@
+import { drawSegment } from "./svgHelper.js";
+import { makeFont, makeCssFontFace } from "./fontHelper.js"
+
+export default {
+  props: {
+    baseUrl: String,
+    name: String,
+    sampleText: String,
+    initSegmentMap: Object,
+    initSegments: Object,
+    initShrink: Number,
+    initGrow: Number,
+    initBevel: Number,
+  },
+  data() {
+    return {
+      segmentMap: this.initSegmentMap,
+      segments: this.initSegments,
+      shrink: this.initShrink,
+      grow: this.initGrow,
+      bevel: this.initBevel,
+    }
+  },
+  computed: {
+    segmentMapAsText() {
+      return Object.entries(this.segmentMap).map(([from, to]) => `${from} ${to}`).join('\n');
+    },
+    font() {
+      console.groupCollapsed('Font paramters')
+      console.log(`segmentMap: ${JSON.stringify(this.segmentMap, null, 2)}`);
+      console.log(`segments: ${JSON.stringify(this.segments).replaceAll(',"', ',\n  "')}`);
+      console.groupEnd();
+      const font = makeFont({
+        fontName: 'spiro-font',
+        segmentMap: this.segmentMap,
+        segments: this.segments,
+        shrink: this.shrink,
+        grow: this.grow,
+        bevel: this.bevel
+      });
+      // Vue does not allow us to include <style> in a template;
+      // Happy to find a better way to do this!
+      document.getElementById('font-face').innerHTML = makeCssFontFace('spiro-font', font);
+      return font;
+    }
+  },
+  methods: {
+    downloadFont() {
+      this.font.download()
+    },
+    textToSegmentMap(event) {
+      const text = event.target.value;
+      const segmentMap = Object.fromEntries(
+        text.split('\n')
+        .map((line) => line.split(/\s+/))
+        .filter(([key, value]) => Boolean(key))
+      );
+      this.segmentMap = {' ': '', ...segmentMap};
+    }
+  },
+  mounted() {
+    const raphaelContainer = this.$refs.raphael;
+    const raphael = Raphael(raphaelContainer, 0, 0, 200, 200).setViewBox(-20, -20, 300, 300);
+    function forceRegen() {
+      // Update from callbacks registered in Raphael were not propagating, so force it:
+      console.log('force regen', this.font)
+    }
+    for (const label in this.segments) {
+      drawSegment(raphael, label, this.segments, forceRegen.bind(this))
+    }
+
+    // Touch this.font to trigger immediate generation:
+    console.log('force regen on mount', this.font);
+  },
+  template: `
+    <p><a :href="baseUrl">home</a></p>
+    <h1>{{ name }}</h1>
+    <textarea rows="2" columns="12" class="style-me">{{ sampleText }}</textarea>
+    <button class="style-me" @click="downloadFont">GET FONT</button>
+    <textarea rows="10" :value="segmentMapAsText" @change="textToSegmentMap" />
+    <label>shrink: <input type="number" v-model.lazy="shrink"></label>
+    <label>grow: <input type="number" v-model.lazy="grow"></label>
+    <label>bevel: <input type="number" v-model.lazy="bevel"></label>
+    <div ref="raphael" />
+  `
+}
---
+++ docs/assets/js/svgHelper.js
@@ -0,0 +1,97 @@
+export function drawSegment(raphael, label, segments, forceRegen) {
+  const vertices = segments[label];
+  const first = vertices[0];
+  const rest = vertices.slice(1);
+  const path = [
+    ['M', ...first],
+    ...rest.map((pair) => ['L', ...pair]),
+    ['Z']
+  ];
+
+  const polygon = raphael.path(path).attr('fill','#444').data('label', label);
+
+  const centroid = findCentroid(vertices);
+  const text = raphael.text(centroid[0], centroid[1], label).attr('fill', '#fff');
+
+  raphael.setStart();
+  for (let i = 0; i < vertices.length; i++) {
+    raphael.circle(...vertices[i], 3).update = function(dx,dy) {
+      const cx = this.attr('cx') + dx;
+      const cy = this.attr('cy') + dy;
+      this.attr({cx, cy});
+      
+      const oldX = path[i][1];
+      const oldY = path[i][2];
+      const newX = round(cx);
+      const newY = round(cy);
+      if (oldX !== newX || oldY !== newY) {
+        path[i][1] = newX;
+        path[i][2] = newY;
+        const vFromPath = path.slice(0,-1).map(triple => [triple[1], triple[2]])
+        const centroid = findCentroid(vFromPath);
+        text.attr({x: centroid[0], y: centroid[1]})
+        polygon.attr({path});
+      }
+    }
+  }
+
+  const controls = raphael.setFinish();
+  controls.attr({fill: '#000', stroke: '#fff'});  
+  controls.drag(onMove, onStart, function() {
+    const cx = round(this.attr('cx'));
+    const cy = round(this.attr('cy'));
+    this.attr({cx, cy});
+    const newSegments = getSegments(raphael)
+    segments[label] = newSegments[label];
+    // TODO: segments is a proxy object...
+    // and I can see the state change in the debugger...
+    // but the UI is not updating!
+    forceRegen()
+  });
+}
+
+function getSegments(raphael) {
+  const segments = {};
+  raphael.forEach((element) => {
+    const label = element.getData()?.label
+    if (!label) return;
+    segments[label] = element.attrs.path.slice(0, -1).map((step) => step.slice(1));
+  });
+  return segments
+}
+
+function findCentroid(vertices) {
+  // Adapted from https://stackoverflow.com/a/33852627
+  let off = vertices[0];
+  let twicearea = 0;
+  let x = 0;
+  let y = 0;
+  for (let i = 0, j = vertices.length - 1; i < vertices.length; j = i++) {
+    const p1 = vertices[i];
+    const p2 = vertices[j];
+    const f = (p1[0] - off[0]) * (p2[1] - off[1]) - (p2[0] - off[0]) * (p1[1] - off[1]);
+    twicearea += f;
+    x += (p1[0] + p2[0] - 2 * off[0]) * f;
+    y += (p1[1] + p2[1] - 2 * off[1]) * f;
+  }
+  const sixArea = twicearea * 3;
+  return [
+    x / sixArea + off[0],
+    y / sixArea + off[1]
+  ];
+}
+
+function onMove(dx,dy) {
+  this.update(dx - this.dx, dy - this.dy);
+  this.dx = dx;
+  this.dy = dy;
+}
+
+function onStart() {
+  this.dx = 0;
+  this.dy = 0;
+}
+
+function round(x) {
+  return Math.round(x/5) * 5
+}
