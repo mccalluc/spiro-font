75e29e7be1a27c1839857dea91d40b4dc3f196e4
mccalluc <mccalluc@users.noreply.github.com>
2022-03-23 09:28:30 -0400

    components directory

--- docs/assets/js/components/MainComponent.js
+++ docs/assets/js/components/MainComponent.js
@@ -1,5 +1,5 @@
-import { drawSegment } from "./svgHelper.js";
-import { makeFont, makeCssFontFace } from "./fontHelper.js"
+import { drawSegment } from "../svgHelper.js";
+import { makeFont, makeCssFontFace } from "../fontHelper.js"
 
 export default {
   props: {
