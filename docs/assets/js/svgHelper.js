39762d88345c5483686f07b5b2aa37755e60bb39
mccalluc <mccalluc@users.noreply.github.com>
2022-03-24 23:25:48 -0400

    Remove label / add click

--- docs/assets/js/svgHelper.js
+++ docs/assets/js/svgHelper.js
@@ -9,10 +9,11 @@ export function drawSegment({raphael, label, segments}) {
   ];
 
   const polygon = raphael.path(path).data('label', label).attr('stroke', '#fff');
+  polygon.click(function(e) {console.log(e)})
   polygon.node.setAttribute('id', `segment-${label}`);
 
-  const centroid = findCentroid(vertices);
-  const text = raphael.text(centroid[0], centroid[1], label).attr('fill', '#fff');
+  // const centroid = findCentroid(vertices);
+  // const text = raphael.text(centroid[0], centroid[1], label).attr('fill', '#fff');
 
   raphael.setStart();
   for (let i = 0; i < vertices.length; i++) {
@@ -28,9 +29,9 @@ export function drawSegment({raphael, label, segments}) {
       if (oldX !== newX || oldY !== newY) {
         path[i][1] = newX;
         path[i][2] = newY;
-        const vFromPath = path.slice(0,-1).map(triple => [triple[1], triple[2]])
-        const centroid = findCentroid(vFromPath);
-        text.attr({x: centroid[0], y: centroid[1]})
+        // const vFromPath = path.slice(0,-1).map(triple => [triple[1], triple[2]])
+        // const centroid = findCentroid(vFromPath);
+        // text.attr({x: centroid[0], y: centroid[1]})
         polygon.attr({path});
       }
     }
@@ -38,44 +39,13 @@ export function drawSegment({raphael, label, segments}) {
   const controls = raphael.setFinish();
 
   controls.attr({fill: '#000', stroke: '#fff'});  
-  controls.drag(onMove, onStart, function() {
+  controls.drag(onMove, onStart, function onEnd() {
     const cx = round(this.attr('cx'));
     const cy = round(this.attr('cy'));
     this.attr({cx, cy});
     const newSegments = getSegments(raphael)
     segments[label] = newSegments[label];
   });
-}
-
-function getSegments(raphael) {
-  const segments = {};
-  raphael.forEach((element) => {
-    const label = element.getData()?.label
-    if (!label) return;
-    segments[label] = element.attrs.path.slice(0, -1).map((step) => step.slice(1));
-  });
-  return segments
-}
-
-function findCentroid(vertices) {
-  // Adapted from https://stackoverflow.com/a/33852627
-  let off = vertices[0];
-  let twicearea = 0;
-  let x = 0;
-  let y = 0;
-  for (let i = 0, j = vertices.length - 1; i < vertices.length; j = i++) {
-    const p1 = vertices[i];
-    const p2 = vertices[j];
-    const f = (p1[0] - off[0]) * (p2[1] - off[1]) - (p2[0] - off[0]) * (p1[1] - off[1]);
-    twicearea += f;
-    x += (p1[0] + p2[0] - 2 * off[0]) * f;
-    y += (p1[1] + p2[1] - 2 * off[1]) * f;
-  }
-  const sixArea = twicearea * 3;
-  return [
-    x / sixArea + off[0],
-    y / sixArea + off[1]
-  ];
 }
 
 function onMove(dx,dy) {
@@ -92,3 +62,34 @@ function onStart() {
 function round(x) {
   return Math.round(x/5) * 5
 }
+
+function getSegments(raphael) {
+  const segments = {};
+  raphael.forEach((element) => {
+    const label = element.getData()?.label
+    if (!label) return;
+    segments[label] = element.attrs.path.slice(0, -1).map((step) => step.slice(1));
+  });
+  return segments
+}
+
+// function findCentroid(vertices) {
+//   // Adapted from https://stackoverflow.com/a/33852627
+//   let off = vertices[0];
+//   let twicearea = 0;
+//   let x = 0;
+//   let y = 0;
+//   for (let i = 0, j = vertices.length - 1; i < vertices.length; j = i++) {
+//     const p1 = vertices[i];
+//     const p2 = vertices[j];
+//     const f = (p1[0] - off[0]) * (p2[1] - off[1]) - (p2[0] - off[0]) * (p1[1] - off[1]);
+//     twicearea += f;
+//     x += (p1[0] + p2[0] - 2 * off[0]) * f;
+//     y += (p1[1] + p2[1] - 2 * off[1]) * f;
+//   }
+//   const sixArea = twicearea * 3;
+//   return [
+//     x / sixArea + off[0],
+//     y / sixArea + off[1]
+//   ];
+// }
