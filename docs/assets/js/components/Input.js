b49c6cbaf99b21d1f157670d647b201a28db3ab0
Chuck McCallum <mccalluc@users.noreply.github.com>
2022-03-30 09:56:39 -0400

    Mccalluc/input component (#35)
    * Checkpoint: partial refactor, not working
    
    * coerce to number and it seems to work
    
    * Explanation

---
+++ docs/assets/js/components/Input.js
@@ -0,0 +1,30 @@
+export default {
+  props: {
+    label: String,
+    modelValue: Number,
+  },
+  emits: ['update:modelValue'],
+  // Vue's default behavior for .number is:
+  //
+  // > If the value cannot be parsed with parseFloat(),
+  // > then the original value is used instead.
+  // https://vuejs.org/guide/essentials/forms.html#number
+  //
+  // We never want a non-numeric value, so instead we use Number() explicitly. 
+  template: `
+    <div class="form-group row py-1">
+      <div class="col-6">
+        <label :for="label" v-html="label" />
+      </div>
+      <div class="col-6">
+        <input
+          type="number"
+          class="form-control"
+          :id="label"
+          :value="modelValue"
+          @input="$emit('update:modelValue', Number($event.target.value))"
+        >
+      </div>
+    </div>
+  `
+}
