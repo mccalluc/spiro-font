c01559272aa82895b229fdc5d2c5408b0590533e
mccalluc <mccalluc@users.noreply.github.com>
2022-03-29 22:10:34 -0400

    more actions

--- cypress/integration/spiro-font.spec.js
+++ cypress/integration/spiro-font.spec.js
@@ -8,6 +8,19 @@ describe('spiro-font', () => {
     cy.contains('Seven Segment').click()
     cy.contains('can be found in patents as early as 1903')
     
-    
+    // <details> visibility not handled.
+    // https://github.com/cypress-io/cypress/issues/20706
+    cy.contains('Getting started').closest('details').should('not.have.attr', 'open')
+    cy.contains('Getting started').click()
+    cy.contains('Getting started').closest('details').should('have.attr', 'open')
+    cy.contains('Getting started').click()
+
+    cy.contains('0123456789')
+
+    cy.get('#shrink').type('{backspace}{backspace}0')
+    cy.get('#grow').type('{backspace}{backspace}0')
+    cy.get('#bevel').type('{backspace}{backspace}0')
+
+    cy.get('select').select('A')
   })
 })
