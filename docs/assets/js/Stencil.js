d22f6c371f20d61a5cf4e66fdf625e3b6c9b60d8
mccalluc <mccalluc@users.noreply.github.com>
2022-03-30 21:03:17 -0400

    Affine transformation: proof of concept

--- docs/assets/js/Stencil.js
+++ docs/assets/js/Stencil.js
@@ -11,13 +11,24 @@ function bufferPolygon(polygon, bufferDistance, cornerCount) {
   return pairs.slice(1); // Start and end points are equal.
 }
 
+function affineTransform(polygon) {
+  const geoJson = { type: 'Polygon', coordinates: [[...polygon, polygon[0]]] };
+  const geometry = reader.read(geoJson);
+  const transformation = new jsts.geom.util.AffineTransformation();
+  transformation.shear(0.2, 0).scale(0.8, 1);
+  const transformed = transformation.transform(geometry);
+  const pairs = transformed._shell._points._coordinates.map((point) => [point.x, point.y])
+  return pairs.slice(1); // Start and end points are equal.
+}
+
 function scale({points, shrink, grow, bevel}) {
   // TODO: Make more robust against bad geometry.
   const scaledUp = points.map((point) => [point[0], 180 - (point[1])]);
   // Shrink the segments away from each other...
   const eroded = bufferPolygon(scaledUp, -shrink, 1);
   // and then expand with rounded corners...
-  return bufferPolygon(eroded, grow, bevel);
+  const expanded = bufferPolygon(eroded, grow, bevel);
+  return affineTransform(expanded);
 }
 
 export default class Stencil {
