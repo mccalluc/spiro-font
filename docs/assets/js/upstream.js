b2cac5bebc135c704ba3408509c84f84402d8214
mccalluc <mccalluc@users.noreply.github.com>
2022-04-03 11:52:27 -0400

    comment

--- docs/assets/js/upstream.js
+++ docs/assets/js/upstream.js
@@ -1,6 +1,10 @@
-// List all external dependencies here.
+// List external dependencies here.
 // - If we need to move to a different CDN, it's all in one place.
 // - Individual files don't need to track the version we're using.
+//
+// Vue is an exception: We want a different version in development and production,
+// and I'm not sure how to get that conditionality in here without breaking mocha,
+// so it's still pulled in as global in the HTML.
 
 export {default as Raphael} from 'https://cdn.skypack.dev/raphael@2.3.0';
 export {default as opentype} from 'https://cdn.skypack.dev/opentype.js@1.3.4';
